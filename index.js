0b223a415bff422892a97612cff699188f5e6b45
Mac <mac@Macs-MacBook-Air.local>
2023-05-03 03:33:18 +0600

    set id wise data for API

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.get('/', (req, res) => {
 app.get('/chefs',(req,res)=>{
     res.send(chefs);
 })
+app.get('/chefs/:id', (req,res) =>{
+    const id = req.params.id;
+    if (id == 0){
+        res.send(chefs)
+    }
+    else{
+        const selectedChef = chefs.find(c => c.id == id);
+        res.send(selectedChef);
+    }
+    
+})
 app.listen(port, () => {
   console.log(`Chef API is running on port: ${port}`)
 })
