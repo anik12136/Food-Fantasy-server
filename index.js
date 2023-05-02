02bc1b5cc1568ff90ae2ed1bc895f921369eaa0b
Mac <mac@Macs-MacBook-Air.local>
2023-05-02 22:58:17 +0600

    use cors for prepare json data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const chefs = require('./Data/chef.json')
+const express = require('express');
+const app = express();
+const cors =require('cors');
+const port = 3000 ;
+const chefs = require('./Data/chef.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Chef is cooking')
