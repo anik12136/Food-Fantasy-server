950fe9d7603d9c6ab85c71706253f060ff9a491e
Mac <mac@Macs-MacBook-Air.local>
2023-05-02 22:16:09 +0600

    seeing data in the local host path

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const chefs = require('./Data/chef.json')
 
 app.get('/', (req, res) => {
   res.send('Chef is cooking')
 })
 
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
+})
 app.listen(port, () => {
   console.log(`Chef API is running on port: ${port}`)
 })
